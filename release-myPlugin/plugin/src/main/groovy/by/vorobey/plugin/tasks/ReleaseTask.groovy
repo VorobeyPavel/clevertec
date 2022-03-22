6a0988f99c563b6aeee633b37539c50e2df2cfd7
VorobeyPavel <Vorobey_Pavel@mail.ru>
2022-03-22 08:04:54 +0300

    first commit

---
+++ release-myPlugin/plugin/src/main/groovy/by/vorobey/plugin/tasks/ReleaseTask.groovy
@@ -0,0 +1,19 @@
+package by.vorobey.plugin.tasks
+
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class ReleaseTask extends DefaultTask {
+
+   /* @TaskAction
+    def release() {
+
+        if (GitUtils.currentBranch == 'master') {
+            new CreateMajorRelease().createMajorRelease()
+        } else {
+            new CreateMinorRelease().createMinorRelease()
+        }
+
+    }*/
+}
